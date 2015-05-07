a2b4868d03bfb1e3159721dfb44e825c0d767507
Anastasios Antoniadis <anastasios.antoniadis@gmail.com>
2015-05-07 18:43:02 +0300

    Possibly ready to test

--- src/TransformerThread.java
+++ src/TransformerThread.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by anantoni on 7/5/2015.
@@ -6,10 +8,22 @@ import java.io.*;
 public class TransformerThread implements Runnable {
     private File fileEntry;
     private String projectFactsDir;
+    private Set<String> libraryFiles;
 
     public TransformerThread(File fileEntry, String projectFactsDir) {
         this.fileEntry = fileEntry;
         this.projectFactsDir = projectFactsDir;
+        this.libraryFiles = new HashSet<>();
+        libraryFiles.add("InitializedClass.facts");
+        libraryFiles.add("ClassInitializer.facts");
+        libraryFiles.add("AssignCompatible.facts");
+        libraryFiles.add("MethodLookup.facts");
+        libraryFiles.add("AssignNormalHeapAllocation.facts");
+        libraryFiles.add("AssignAuxiliaryHeapAllocation.facts");
+        libraryFiles.add("AssignContextInsensitiveHeapAllocation.facts");
+        libraryFiles.add("MainMethodDeclaration.facts");
+        libraryFiles.add("ImplicitReachable.facts");
+
     }
 
     public static boolean isInteger(String s) {
@@ -41,25 +55,37 @@ public class TransformerThread implements Runnable {
             e.printStackTrace();
         }
         String line;
-        String predicateName = fileEntry.getName().replace(".facts", "").replace("-", ":");
-        StringBuilder transformedArgs = new StringBuilder();
+        String predicateName = fileEntry.getName().replace(".facts", "").replace("-", "");
+
 
         try {
+            int groups = 0;
             while ((line = factsReader.readLine()) != null) {
-                String[] predicateArgs = line.split("\t");
+                line = line.trim();
 
+                String[] predicateArgs;
+                if (libraryFiles.contains(fileEntry.getName()))
+                    predicateArgs = line.split("\t");
+                else
+                    predicateArgs = line.split(", ");
+                if (predicateArgs.length > groups || predicateArgs.length < groups && groups > 0)
+                    continue;
+                else if (groups == 0)
+                    groups = predicateArgs.length;
+
+                StringBuilder transformedArgs = new StringBuilder();
                 for(int i = 0; i < predicateArgs.length; i++) {
                     if (i == 0) {
                         if (isInteger(predicateArgs[i]))
                             transformedArgs.append(predicateArgs[i]);
                         else
-                            transformedArgs.append(predicateArgs[i]);
+                            transformedArgs.append("\"" + predicateArgs[i] + "\"");
                     }
                     else {
                         if (isInteger(predicateArgs[i]))
                             transformedArgs.append("," + predicateArgs[i]);
                         else
-                            transformedArgs.append(",\'" + predicateArgs[i] + "\'");
+                            transformedArgs.append(",\"" + predicateArgs[i] + "\"");
                     }
 
                 }
